9cca295d31385fb51823790bd848d1c41f938ce3
sunshinemoua <sunshinemoua21@gmail.com>
2024-07-10 17:20:52 -0700

    added bottom tab navigation

--- App.js
+++ App.js
@@ -6,45 +6,28 @@ import {
   KeyboardAvoidingView,
   Platform,
 } from "react-native";
-import HomeScreen from "./components/HomeScreen";
+import HomeScreen from "./components/screens/HomeScreen";
 import tw from "tailwind-react-native-classnames";
 import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import store from "./redux/store";
 import { createStackNavigator } from "@react-navigation/stack";
+import NavOptions from "./components/NavOptions";
 
 export default App = () => {
   const Stack = createStackNavigator();
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <SafeAreaProvider>
-          <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
-            style={tw`flex-1`}
-          >
-            <Stack.Navigator>
-              <Stack.Screen
-                name="HomeScreen"
-                component={HomeScreen}
-                options={{ headerShown: false }}
-              />
-              {/* <Stack.Screen
-                name="MapScreen"
-                component={MapScreen}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="EatsScreen"
-                component={EatsScreen}
-                options={{ headerShown: false }}
-              /> */}
-            </Stack.Navigator>
-          </KeyboardAvoidingView>
-        </SafeAreaProvider>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+          style={tw`flex-1`}
+        >
+          <NavOptions />
+        </KeyboardAvoidingView>
+      </SafeAreaProvider>
     </Provider>
   );
 };
